48e58650d39b41e3883fb43cf754c2f00b466d24
Andres Munevar <afmunevarm@hotmail.com>
2020-08-04 14:33:57 -0400

    Weather App

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CityWeatherComponent } from './city-weather/city-weather.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CityWeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/city-weather/city-weather.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
+import { observable } from 'rxjs';
+
+@Component({
+  selector: 'app-city-weather',
+  templateUrl: './city-weather.component.html',
+  styleUrls: ['./city-weather.component.css']
+})
+export class CityWeatherComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void { }
+
+  weatherApi: Object;
+  cityExists: Boolean = false;
+  errorMsg: String;
+  //To hide error element and its styles on page load
+  errorExist: Boolean = false;
+
+  searchForm = new FormGroup({
+    cityName: new FormControl('')
+  });
+  //Retrieve form input
+  onSubmit(formValue){
+    this.getData(formValue.cityName);
+  }
+  //Pull the weather data of an specific city from the API 
+  getData(cityName: String){
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" 
+              + cityName + "&appid=af26e4a70cbde819abfc5d8662b3094d&units=metric";
+    let obs = this.http.get<String>(url);
+    this.validateSearch(obs);
+  }
+  //Check wether the city is found or not
+  validateSearch(observable){
+    observable.subscribe(
+      (data: Object) => {
+        //If the city is found, then assign its data to weatherApi
+        this.weatherApi = data;
+        this.cityExists = true;
+        this.errorExist = false;
+      }, 
+      (e: any) => {
+        //If there's an error, then display the error message
+        this.errorMsg = "Opps, " + e.error.message;
+        this.cityExists = false;
+        this.errorExist = true;
+      }
+    );
+  }
+
+}
